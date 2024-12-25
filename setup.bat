213a4c2a74c3a29aa0afea206928799e64189ebd
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2024-12-25 23:49:57 +0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 @echo off
-title name Bot
+title Somnia Network Devnet Bot
 color 0A
 
 cd ..
@@ -14,7 +14,7 @@ if exist node_modules (
 :MENU
 cls
 echo =================================================================
-echo    name BOT SETUP AND RUN SCRIPT
+echo    Somnia Network Devnet BOT SETUP AND RUN SCRIPT
 echo =================================================================
 echo.
 echo Current directory: %CD%
